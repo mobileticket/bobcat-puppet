a05118c5f3d11634783c8680cc32727c136fc113
Per Norling <47216062+6167prng@users.noreply.github.com>
2025-02-11 14:51:53 +0100

    Update pki.pp

--- manifests/os/pki.pp
+++ manifests/os/pki.pp
@@ -2,7 +2,7 @@ class bobcat::os::pki (
   $ca_name   = "Puppet CA",
   $ca_source = "file:///etc/puppet/ssl/certs/ca.pem"
 ){
-  require ca_cert  # https://forge.puppet.com/modules/puppet/ca_cert/
+  #require ca_cert  # https://forge.puppet.com/modules/puppet/ca_cert/
 
   ca_cert::ca {
     $ca_name:
