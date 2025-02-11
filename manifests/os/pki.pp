52e18f743a32b8aab25555b8e315cee439148482
Per Norling <47216062+6167prng@users.noreply.github.com>
2025-02-11 16:54:22 +0100

    Update pki.pp
    Reworked to remove a implicit loop in deb12.

--- manifests/os/pki.pp
+++ manifests/os/pki.pp
@@ -1,12 +1,12 @@
 class bobcat::os::pki (
   $ca_name   = "Puppet CA",
   $ca_source = "file:///etc/puppet/ssl/certs/ca.pem"
-){
-  #require ca_cert  # https://forge.puppet.com/modules/puppet/ca_cert/
+) {
+  include ca_cert  # Ensures the class is present, without forcing a strict dependency
 
-  ca_cert::ca {
-    $ca_name:
-      ensure => 'trusted',
-      source => "${ca_source}";
+  ca_cert::ca { $ca_name:
+    ensure => 'trusted',
+    source => $ca_source,
+    require => Class['ca_cert'],  # Avoids an implicit loop
   }
 }
