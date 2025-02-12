8c9b318d6b21d90d38c5d91cadd2db6e99025274
Per Norling <47216062+6167prng@users.noreply.github.com>
2025-02-12 12:17:03 +0100

    Update pki.pp

--- manifests/os/pki.pp
+++ manifests/os/pki.pp
@@ -1,12 +1,12 @@
 class bobcat::os::pki (
   $ca_name   = "Puppet CA",
   $ca_source = "file:///etc/puppet/ssl/certs/ca.pem"
-) {
-  include ca_cert  # Ensure the class is included, but without forcing strict ordering
+){
+#  require ca_cert  # https://forge.puppet.com/pcfens/ca_cert
 
-  ca_cert::ca { $ca_name:
-    ensure => 'trusted',
-    source => $ca_source,
-    before => Exec['ca_cert_update'],  # Ensures ca_cert::ca runs first
+  ca_cert::ca {
+    $ca_name:
+      ensure => 'trusted',
+      source => "${ca_source}";
   }
 }
