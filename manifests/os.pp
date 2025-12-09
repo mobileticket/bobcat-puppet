7e7ab44e15c5e4361b018afb9232fd17ce5b0745
Per Norling <47216062+6167prng@users.noreply.github.com>
2025-12-09 08:21:54 +0100

    Update os.pp

--- manifests/os.pp
+++ manifests/os.pp
@@ -5,7 +5,7 @@ class bobcat::os (
   include ::bobcat::os::rng
   include ::bobcat::os::ntp
   include ::bobcat::os::syslog
-#  include ::bobcat::os::pki
+  include ::bobcat::os::pki
 
   create_resources(user, $users, { ensure => present })
   create_resources(ssh_authorized_key, $ssh_authorized_keys, { ensure => present })
